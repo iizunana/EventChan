ab15e3286b8e9b68054dd4b83d71b93d4867db66
Ryo Ishida <131y005@gmail.com>
2015-06-19 08:01:16 +0000

    last

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,11 @@ class PagesController < ApplicationController
   
   def setname
 #    logger.info current_user.inspect
-<<<<<<< HEAD
+
     @user = current_user
     @join_events = @user.join_events.all
     @join_event = @user.join_events.build
-=======
-    if @user.nil? then
-      @user = current_user
-    end
->>>>>>> 0fd1715fcf1a3b22801dde4b21a0a05654968526
+    
   end
   
   def update
